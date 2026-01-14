715ed476f7deaa320991a78a6ac89c4c13e7b28d
amtellezfernandez <amtellezfernandez@gmail.com>
2026-01-14 11:20:04 +0100

    Revert "fix: point Tailwind to apps/web config"
    This reverts commit 559f44a673e6c0201afef2aa9d778b550edb8b23.

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,13 +1,6 @@
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 export default {
   plugins: {
-    tailwindcss: {
-      config: join(__dirname, "tailwind.config.ts"),
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
